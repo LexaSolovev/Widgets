3131c6c3838b2d1417af5102673b9d0d0713a3d5
Alexey Solovyev <solovaa@yandex.ru>
2024-12-25 22:01:11 +0000

    init project, add linters, add masks

---
+++ main.py
@@ -0,0 +1,4 @@
+from src.masks import get_mask_account, get_mask_card_number
+
+print(get_mask_card_number("1234567812345678"))
+print(get_mask_account("1234123412356567"))
---
+++ src/masks.py
@@ -0,0 +1,14 @@
+def get_mask_card_number(card_number: str) -> str:
+    """Функция get_mask_card_number
+     принимает на вход номер карты и возвращает ее маску. Номер карты замаскирован и отображается в формате
+    XXXX XX** **** XXXX
+    """
+    return card_number[:4] + " " + card_number[4:6] + "** **** " + card_number[-4:]
+
+
+def get_mask_account(account_number: str) -> str:
+    """Функция
+    get_mask_account
+     принимает на вход номер счета и возвращает его маску. Номер счета замаскирован и отображается в формате
+    **XXXX"""
+    return "**" + account_number[-4:]
