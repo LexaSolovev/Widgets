a5d003b3fbeab617eed55a00160d22b57f60b48b
Alexey Solovyev <solovaa@yandex.ru>
2025-01-16 20:58:56 +0000

    add test func get_mask_account

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -5,6 +5,13 @@ from src.masks import get_mask_account, get_mask_card_number
 def card_number():
     return "1234 5678 9876 5432"
 
+@pytest.fixture
+def account():
+    return "1234567898765432"
 
 def test_get_mask_card_number(card_number):
     assert get_mask_card_number(card_number) == "1234 56** **** 5432"
+
+
+def test_get_mask_account(account):
+    assert get_mask_account(account) == "**5432"
