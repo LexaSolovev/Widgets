77c0265ea273f5fdc6d66b684b90e61c87e81536
Alexey Solovyev <solovaa@yandex.ru>
2025-01-18 21:42:56 +0000

    delete excess fixtures

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,16 +1,9 @@
 import pytest
 from src.masks import get_mask_account, get_mask_card_number
 
-@pytest.fixture
-def card_number():
-    return "1234567898765432"
 
-@pytest.fixture
-def account():
-    return "123456789987654321"
-
-def test_get_mask_card_number(card_number):
-    assert get_mask_card_number(card_number) == "1234 56** **** 5432"
+def test_get_mask_card_number():
+    assert get_mask_card_number("1234567898765432") == "1234 56** **** 5432"
 
 
 def test_get_mask_account(account):
