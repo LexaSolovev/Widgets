fdb5954f45fd079f0142dbb33a4da6813b295c0c
Alexey Solovyev <solovaa@yandex.ru>
2025-01-24 22:33:32 +0000

    add module generators.py add head of func

---
+++ src/generators.py
@@ -0,0 +1,10 @@
+def filter_by_currency(transactions: list[dict], currency: str) -> list[dict]:
+    pass
+
+
+def transaction_descriptions(transactions):
+    pass
+
+
+def card_number_generator(start, stop):
+    pass
