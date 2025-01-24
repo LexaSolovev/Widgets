2f8089997684de98cf1a59ad7d8909ea934b7302
Alexey Solovyev <solovaa@yandex.ru>
2025-01-24 23:15:00 +0000

    reliz func filter_by_corrency

--- src/generators.py
+++ src/generators.py
@@ -1,5 +1,8 @@
-def filter_by_currency(transactions: list[dict], currency: str) -> list[dict]:
-    pass
+def filter_by_currency(transactions: list[dict], currency_code: str):
+    generator = (transaction for transaction in transactions if transaction.get("operationAmount").get("currency").get("code") == currency_code)
+    for transaction in generator:
+        yield dict(transaction)
+
 
 
 def transaction_descriptions(transactions):
