ede1b1c175ac208a241247819d6f913d0434f70c
Alexey Solovyev <solovaa@yandex.ru>
2025-01-25 11:26:25 +0000

    add annotation and docstring to filter_by_currency

--- src/generators.py
+++ src/generators.py
@@ -1,5 +1,11 @@
-def filter_by_currency(transactions: list[dict], currency_code: str):
-    #Проверяем что тип входных данных соответсвует ожиданиям и список транзакций не пустой
+from typing import Generator
+
+def filter_by_currency(transactions: list[dict], currency_code: str) -> Generator[dict]:
+    """
+    Функция принимает список транзакций и возвращает генератор,
+    который поочередно возвращает транзакции с определенной валютой
+    """
+
     generator = (transaction for transaction in transactions if transaction.get("operationAmount",{}).get("currency",{}).get("code") == currency_code)
     for transaction in generator:
         yield transaction
