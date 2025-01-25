0df6d21f776a6a62b879aa27409cad9b3d119c5a
Alexey Solovyev <solovaa@yandex.ru>
2025-01-25 13:16:57 +0000

    fix

--- src/generators.py
+++ src/generators.py
@@ -18,12 +18,12 @@ def transaction_descriptions(transactions: list[dict]) -> Generator[str, None, N
 
 def card_number_generator(start: int, stop: int) -> Generator[str, None, None]:
     result_str = "0123 4567 8901 2345"
-    nums = (convert_card_number('{:0{length}}'.format(x, length=16)) for x in range(start, stop+1))
+    nums = (format_card_number('{:0{length}}'.format(x, length=16)) for x in range(start, stop + 1))
     for num in nums:
         yield num
 
 
-def convert_card_number(input_str: str) -> str:
+def format_card_number(input_str: str) -> str:
     """Преобразует строку вида "1234567812345678" в строку номера карты - "1234 5678 1234 5678" """
     return input_str[:4] + " " + input_str[4:8] + " " + input_str[8:12] + " " + input_str[12:]
 
