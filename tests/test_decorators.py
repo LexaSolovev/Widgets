caa939309bbdb2320ab6df615faebd6e84706e8a
Alexey Solovyev <solovaa@yandex.ru>
2025-02-04 19:39:19 +0000

    add test log_console with exeption

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -5,9 +5,21 @@ def test_log_console(capsys):
     def summator(*args):
         return sum(args)
 
-    summator(1, 2, 3, 4)
+    result = summator(1, 2, 3, 4)
     str_in_console = capsys.readouterr()
+    assert result == 10
     assert "Начало запуска функции summator:" in str_in_console.out
     assert "Функция выполнена успешно за" in str_in_console.out
     assert "Результат выполнения функции:" in str_in_console.out
 
+
+def test_log_console_exсept(capsys):
+    @log()
+    def exeptor():
+        raise Exception("Тестовая ошибка")
+
+
+    try:
+        exeptor()
+    except:
+        assert "При выполнении функции exeptor c параметрами () произошла ошибка:\nТестовая ошибка" in capsys.readouterr().out
