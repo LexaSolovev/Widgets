e31fe3ad068e151b038983194137ab78b2d925d2
Alexey Solovyev <solovaa@yandex.ru>
2025-02-04 20:22:18 +0000

    fix after linters

--- src/decorators.py
+++ src/decorators.py
@@ -1,26 +1,35 @@
+from datetime import datetime
 from functools import wraps
 from time import time
-from typing import Callable
-from datetime import datetime
+from typing import Callable, Any
 
-def log(file_name: str="") -> Callable:
+
+def log(file_name: str = ""):
+    """
+    Декоратор логирует выполнение функции.
+    Если указан параметр file_name, то создается или дописывается файл с этим именем
+    Если параметр не указан, то логирование осуществляется в консоль
+    """
+
     def inner(func):
-        def write_to_log(log_str: str, name_of_file:str = ""):
+        def write_to_log(log_str: str, name_of_file: str = "") -> None:
             if file_name == "":
-                print(log_str,end="")
+                print(log_str, end="")
             else:
                 with open(file_name, "a", encoding="UTF-8") as log_file:
                     log_file.write(log_str + "\n")
-
 
         @wraps(func)
         def wrapper(*args, **kwargs):
             write_to_log(f"Начало запуска функции {func.__name__}: {str(datetime.now())}\n", name_of_file=file_name)
             start = time()
             try:
-                result = func(*args,**kwargs)
+                result = func(*args, **kwargs)
             except Exception as exept:
-                write_to_log(f"При выполнении функции {func.__name__} c параметрами {args} произошла ошибка:\n" + str(exept), name_of_file=file_name)
+                write_to_log(
+                    f"При выполнении функции {func.__name__} c параметрами {args} произошла ошибка:\n" + str(exept),
+                    name_of_file=file_name,
+                )
                 raise exept
             else:
                 stop = time()
