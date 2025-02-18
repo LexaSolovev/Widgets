e1c7aa8535610887ca9acb841c93abdbca79fe94
Alexey Solovyev <solovaa@yandex.ru>
2025-02-18 21:42:09 +0000

    fix utills.py

--- src/utills.py
+++ src/utills.py
@@ -1,7 +1,7 @@
 import json
 import os
 from config import PATH_DATA
-from external_api import convert_amount_by_currency
+from src.external_api import convert_amount_by_currency
 
 def get_transactions_from_json(path_to_json:str) -> list[dict]:
     """Функция принимает путь до JSON файла и возвращает данные о транзакциях в виде списка словарей"""
@@ -22,8 +22,3 @@ def get_amount(transaction:dict, currency:str="RUB") -> float:
         return transaction_amount
     else:
         return convert_amount_by_currency(transaction_currency, currency, transaction_amount)
-
-
-if __name__ == "__main__":
-    operations_path = os.path.join(PATH_DATA, "operations.json")
-    print(get_transactions_from_json(operations_path))
