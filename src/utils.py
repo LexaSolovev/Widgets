b11b40b5616fccdddc01a18f9bb8160ca5c63419
Alexey Solovyev <solovaa@yandex.ru>
2025-02-20 22:12:24 +0000

    add func get_transactions_from_excel

--- src/utils.py
+++ src/utils.py
@@ -2,6 +2,7 @@ import json
 import logging
 import os
 import csv
+import pandas as pd
 
 from config import PATH_DATA, PATH_LOGS
 from src.external_api import convert_amount_by_currency
@@ -44,12 +45,19 @@ def get_amount(transaction: dict, currency: str = "RUB") -> float:
         return convert_amount_by_currency(transaction_currency, currency, transaction_amount)
 
 
-def get_transaction_from_csv(path_to_csv: str) -> list[dict]:
+def get_transactions_from_csv(path_to_csv: str) -> list[dict]:
     """Функция принимает путь до CSV файла и возвращает данные о транзакциях в виде списка словарей"""
 
     with open(path_to_csv) as csv_file:
         reader = csv.DictReader(csv_file, delimiter=';')
         return list(reader)
+
+
+def get_transactions_from_excel(path_to_excel: str) -> list[dict]:
+    """Функция принимает путь до EXCEL файла и возвращает данные о транзакциях в виде списка словарей"""
+
+    transactions_df = pd.read_excel(path_to_excel, )
+    return json.loads(transactions_df.to_json(orient='records', force_ascii='list[dict]'))
 
 
 
@@ -58,6 +66,9 @@ if __name__ == "__main__":
     # path_to_json = os.path.join(PATH_DATA, "operations.json")
     # transactions = get_transactions_from_json(path_to_json)
     # print(get_amount(transactions[1]))
-    path_to_csv = os.path.join(PATH_DATA, "transactions.csv")
-    print(get_transaction_from_csv(path_to_csv)[0:4])
+    # path_to_csv = os.path.join(PATH_DATA, "transactions.csv")
+    # print(get_transactions_from_csv(path_to_csv)[0:4])
+    path_to_excel = os.path.join(PATH_DATA, "transactions_excel.xlsx")
+    print(get_transactions_from_excel(path_to_excel)[0:4])
+
 
