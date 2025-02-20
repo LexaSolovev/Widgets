e8234532a76ecda55929fa2e9eb1589dfadb28fa
Alexey Solovyev <solovaa@yandex.ru>
2025-02-20 21:44:38 +0000

    add func get_transaction_from_csv

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,7 @@
 import json
 import logging
 import os
+import csv
 
 from config import PATH_DATA, PATH_LOGS
 from src.external_api import convert_amount_by_currency
@@ -43,7 +44,20 @@ def get_amount(transaction: dict, currency: str = "RUB") -> float:
         return convert_amount_by_currency(transaction_currency, currency, transaction_amount)
 
 
+def get_transaction_from_csv(path_to_csv: str) -> list[dict]:
+    """Функция принимает путь до CSV файла и возвращает данные о транзакциях в виде списка словарей"""
+
+    with open(path_to_csv) as csv_file:
+        reader = csv.DictReader(csv_file, delimiter=';')
+        return list(reader)
+
+
+
+
 if __name__ == "__main__":
-    path_to_json = os.path.join(PATH_DATA, "operations.json")
-    transactions = get_transactions_from_json(path_to_json)
-    print(get_amount(transactions[1]))
+    # path_to_json = os.path.join(PATH_DATA, "operations.json")
+    # transactions = get_transactions_from_json(path_to_json)
+    # print(get_amount(transactions[1]))
+    path_to_csv = os.path.join(PATH_DATA, "transactions.csv")
+    print(get_transaction_from_csv(path_to_csv)[0:4])
+
