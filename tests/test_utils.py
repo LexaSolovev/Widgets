52e452cba534d63ac877b3d303bc8ed41e5eee96
Alexey Solovyev <solovaa@yandex.ru>
2025-02-22 08:01:28 +0000

    fix for linterts

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -6,7 +6,7 @@ from src.utils import get_amount
 
 def test_get_amount_usd():
     mock_amount = Mock(return_value=822137.0)
-    src.utills.convert_amount_by_currency = mock_amount
+    src.utils.convert_amount_by_currency = mock_amount
     assert get_amount(
         {
             "operationAmount":
@@ -24,7 +24,7 @@ def test_get_amount_usd():
 
 def test_get_amount_rub():
     mock_amount = Mock(return_value=822137.0)
-    src.utills.convert_amount_by_currency = mock_amount
+    src.utils.convert_amount_by_currency = mock_amount
     assert get_amount(
         {
             "operationAmount":
