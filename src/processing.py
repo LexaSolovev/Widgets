c901e1e29f454cbed1d716fa9820198709b3c08e
Alexey Solovyev <solovaa@yandex.ru>
2025-02-24 09:13:49 +0000

    add func filter_by_description

--- src/processing.py
+++ src/processing.py
@@ -1,3 +1,6 @@
+import re
+
+
 def filter_by_state(data: list[dict], state: str = 'EXECUTED') -> list[dict]:
     """ Функция принимает список словарей и возвращает список словарей соответствующих состоянию state"""
     result = []
@@ -10,3 +13,20 @@ def filter_by_state(data: list[dict], state: str = 'EXECUTED') -> list[dict]:
 def sort_by_date(data: list[dict], reverse_option: bool = True) -> list[dict]:
     """Функция сортирует список словарей по дате"""
     return sorted(data, key=lambda x: x['date'], reverse=reverse_option)
+
+
+def filter_by_description(transactions: list[dict], search_str: str) -> list[dict]:
+    """
+    Функция принимает список транзакций transactions и строку поиска search_str,
+    возвращает список транзакций, у которых описание содержит строку поиска
+    """
+    search_pattern = rf".*{search_str.lower()}.*"
+    search_reg = re.compile(pattern=search_pattern)
+    result = []
+
+    for transaction in transactions:
+        description = transaction.get("description","")
+        if search_reg.match(description):
+            result.append(transaction)
+
+    return result
